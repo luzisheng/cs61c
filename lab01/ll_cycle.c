91701afbb0ec2746a15457cc7e74f30a34b8c4c0
luzisheng <18554393368@163.com>
2023-03-02 16:55:41 +0800

    done lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,22 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+	/* your code here */
+	node *tortoise = head;
+	node *hare = head;
+	while (hare) {
+		
+		if (!hare->next) {
+			return 0;
+		}
+
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (hare == tortoise) {
+			return 1;
+		}
+	}
+
+	return 0;
 }
